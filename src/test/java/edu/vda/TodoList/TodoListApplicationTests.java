7197a15647e2cf97dd7f3f5addd92d122953da2a
Veronica <almeida.vrc@gmail.com>
2023-10-09 13:07:24 -0300

    First commit

---
+++ src/test/java/edu/vda/TodoList/TodoListApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.vda.TodoList;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
