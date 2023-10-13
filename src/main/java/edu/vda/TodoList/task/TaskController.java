f8b5567d40631d84dbb990706dbd186314b69dfe
Veronica <almeida.vrc@gmail.com>
2023-10-13 19:36:22 -0300

    Add get Tasks by uerId

--- src/main/java/edu/vda/TodoList/task/TaskController.java
+++ src/main/java/edu/vda/TodoList/task/TaskController.java
@@ -4,12 +4,10 @@ import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
+import java.util.List;
 import java.util.UUID;
 
 @RestController
@@ -38,5 +36,12 @@ public class TaskController {
 
         var task = this.taskRepository.save(taskModel);
         return ResponseEntity.status(HttpStatus.CREATED).body(task);
+    }
+
+    @GetMapping("/")
+    public List<TaskModel> list(HttpServletRequest request) {
+        var idUser = request.getAttribute(("idUser"));
+        var tasks = this.taskRepository.findByIdUser((UUID) idUser);
+        return tasks;
     }
 }
--- src/main/java/edu/vda/TodoList/task/TaskRepository.java
+++ src/main/java/edu/vda/TodoList/task/TaskRepository.java
@@ -2,7 +2,9 @@ package edu.vda.TodoList.task;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface TaskRepository extends JpaRepository<TaskModel, UUID> {
+    List<TaskModel>findByIdUser(UUID idUSer);
 }
