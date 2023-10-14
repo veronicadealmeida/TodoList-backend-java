c5662fdefc67dbf1a898dc22b5a83da8b36b4520
Veronica <almeida.vrc@gmail.com>
2023-10-14 00:29:13 -0300

    fix CORS

--- src/main/java/edu/vda/TodoList/TodoListApplication.java
+++ src/main/java/edu/vda/TodoList/TodoListApplication.java
@@ -1,8 +1,11 @@
 package edu.vda.TodoList;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@OpenAPIDefinition(servers = { @Server(url = "/", description = "Default Server url")})
 @SpringBootApplication
 public class TodoListApplication {
 
